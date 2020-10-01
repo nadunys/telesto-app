33ff86068bfbdc1ee7d3b7acc1093de1cc05277d
nadunys <yashnadun@gmail.com>
2020-10-01 14:54:07 +0530

    globe added

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react";
+import Globe from "react-globe.gl";
+import "./ExploreContainer.css";
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const { useState, useEffect, useRef } = React;
+    const globeEl = useRef();
+    const [popData, setPopData] = useState([]);
+
+    useEffect(() => {}, []);
+
+    useEffect(() => {}, []);
+
+    const weightColor = null;
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <Globe
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+  
+        hexBinPointsData={popData}
+        hexBinPointWeight="pop"
+        hexAltitude={d => d.sumWeight * 6e-8}
+        hexBinResolution={4}
+        // hexTopColor={d => weightColor(d.sumWeight)}
+        // hexSideColor={d => weightColor(d.sumWeight)}
+        hexBinMerge={true}
+        enablePointerInteraction={false}
+      />
     </div>
   );
 };
