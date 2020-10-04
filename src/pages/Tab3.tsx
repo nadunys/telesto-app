c55e8e1230bda65ff2b66a965b4620a754d5f274
Jayan Jayatiss <Jayan19991212@users.noreply.github.com>
2020-10-04 09:25:43 +0530

    tab3 ui

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonIcon, IonChip, IonLabel, IonItemGroup, IonItemDivider, IonItem, IonBackButton, IonButton, IonMenuButton,  IonItemOptions, IonItemOption, IonButtons, IonList, IonCol} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonIcon, IonChip, IonLabel, IonItemGroup, IonItemDivider, IonItem, IonBackButton, IonButton, IonMenuButton,  IonItemOptions, IonItemOption, IonButtons, IonList, IonCol, IonCardHeader} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
   return (
-    <IonPage>
 
+    <IonPage>
+  
     <IonHeader>
       <IonToolbar>
         <h2>About Us</h2><br />
@@ -16,7 +17,8 @@ const Tab3: React.FC = () => {
       </IonToolbar>
     </IonHeader>
 
-      <IonContent fullscreen>
+      <IonContent>
+      <IonCol>
 
       <h1>name of app</h1>
 
@@ -33,17 +35,17 @@ const Tab3: React.FC = () => {
       </IonItemDivider>
 
       <h5>The Short</h5>
-      <IonItem>
+      
       <p>Main perpose</p>
-      </IonItem>
+      
 
       <h5>The long</h5>
-      <IonItem>
+      
       <p>Full description</p>
-      </IonItem>
+      
 
       <h5>Get in touch</h5>
-      <IonItem>
+      
       <p>Mail us<br/>jayan.jayatissa@gmail.com<br/>
       <br/>Contact us
       <br/>+94719610650<br/>
@@ -54,12 +56,13 @@ const Tab3: React.FC = () => {
       <br/>Third line
       </IonCol>
       </p>
-      </IonItem>
+      
 
       
       </IonItemGroup>
-
+      </IonCol>
       </IonContent>
+      
     </IonPage>
   );
 };
